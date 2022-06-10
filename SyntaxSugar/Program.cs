c4bab64b5255ecee5ab72a4401d0a324acc9f1d8
Febreeze75 <febreezebooz@gmail.com>
2022-06-10 13:15:10 -0400

    initial commit

--- SyntaxSugar/Program.cs
+++ SyntaxSugar/Program.cs
@@ -6,7 +6,19 @@ namespace SyntaxSugar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+            }
+            
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
 }
